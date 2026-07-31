944fca31b5eb0ad4d1af3b05e88197cd81370c66
euler.amorim <euler.amorim@hotmail.com>
2026-07-31 15:57:36 -0300

    feat: add page, header component, seed posts, and image assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-import clsx from 'clsx';
+import { Header } from '@/components/Header';
 
-export default function HomePage() {
+export default async function HomePage() {
+  console.log('HomePage');
   return (
     <div>
-      <h1
-        className={clsx(
-          'text-6xl',
-          'font-bold',
-          'text-blue-500',
-          'hover:text-blue-50',
-          'hover:bg-blue-500',
-          'transition',
-          'duration-1000',
-        )}
-      >
-        Texto
-      </h1>
+      <Header />
     </div>
   );
 }
