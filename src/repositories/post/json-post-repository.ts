ede0da2fcf6adc6ce69640ea9c2120e64c7c8539
euler.amorim <euler.amorim@hotmail.com>
2026-08-04 14:54:16 -0300

    feat: update editor settings and add post repository files

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,12 +1,39 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
+import { resolve } from 'path';
+import { readFile as readfile } from 'fs/promises';
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  'src',
+  'db',
+  'seed',
+  'posts.json',
+);
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readfile(JSON_POSTS_FILE_PATH, 'utf-8');
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
 
-  async findAll(): Promise<PostModel[]> {}
+    return posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+
+    if (!post) throw new Error('Post não encontrado');
+
+    return post;
+  }
 }
 
-export const postRepository = new JsonPostRepository();
+export const postRepository: PostRepository = new JsonPostRepository();
