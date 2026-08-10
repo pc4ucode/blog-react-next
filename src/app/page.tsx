d8ce7a2ed64dc148871c98c5e1ee367c12b6650b
euler.amorim <euler.amorim@hotmail.com>
2026-08-10 17:01:05 -0300

    feat: add posts list component and related UI updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import { Header } from '@/components/Header';
+import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
+import { postRepository } from '@/repositories/post';
+import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <div>
-      <SpinLoader containerClasses='min-h-[500px] bg-amber-500' />
+      <header>
+        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a HEADER</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a FOOTER</h1>
+      </footer>
     </div>
   );
 }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -12,7 +12,14 @@ const JSON_POSTS_FILE_PATH = resolve(
   'posts.json',
 );
 
+const SIMULATE_WAIT_IN_MS = 5000;
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
+
+    await new Promise(resolve => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
   private async readFromDisk(): Promise<PostModel[]> {
     const jsonContent = await readfile(JSON_POSTS_FILE_PATH, 'utf-8');
     const parsedJson = JSON.parse(jsonContent);
@@ -22,11 +29,15 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     return posts;
   }
 
   async findById(id: string): Promise<PostModel> {
+    await this.simulateWait();
+
     const posts = await this.findAll();
     const post = posts.find(post => post.id === id);
 
