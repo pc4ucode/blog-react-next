9bb3d6bacb953014e894bb0167518cbaa008275c
euler.amorim <euler.amorim@hotmail.com>
2026-09-04 09:13:46 -0300

    feat: Add a posts section and update the Header component to use Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,32 @@ import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+            className='group-hover:scale-105 transition duration-300'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='Titulo do Post'
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint iure
+          commodi inventore sapiente. Similique minima ipsam perspiciatis illo
+          iste, molestias inventore. Sapiente, veritatis. Necessitatibus odit
+          nemo sapiente magni ipsum eaque?
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
