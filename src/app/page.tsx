d3160c954b338e64efccf360d725d53644440b51
euler.amorim <euler.amorim@hotmail.com>
2026-09-04 09:54:23 -0300

    feat: Add featured post details to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,30 @@ export default async function HomePage() {
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
         <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
           <Image
-            className='group-hover:scale-105 transition duration-300'
+            className='w-full h-full object-cover object-center group-hover:scale-105 transition duration-300'
             src='/images/bryen_0.png'
             width={1200}
             height={720}
             alt='Titulo do Post'
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint iure
-          commodi inventore sapiente. Similique minima ipsam perspiciatis illo
-          iste, molestias inventore. Sapiente, veritatis. Necessitatibus odit
-          nemo sapiente magni ipsum eaque?
+        <div className='flex flex-col gap-4 sm:justify-center'>
+          <time
+            className='text-slate-600 block text-sm/tight'
+            dateTime='2025-04-20'
+          >
+            20/04/2025 - 10:00
+          </time>
+          <h1 className='text-2xl/tight font-extrabold sm:text-4xl'>
+            <Link href='#'>Titulo do Post</Link>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint iure
+            commodi inventore sapiente. Similique minima ipsam perspiciatis illo
+            iste, molestias inventore. Sapiente, veritatis. Necessitatibus odit
+            nemo sapiente magni ipsum eaque?
+          </p>
         </div>
       </section>
 
