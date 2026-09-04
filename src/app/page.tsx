f7b752c6394bf9374bb78a906728d33f240a6b08
euler.amorim <euler.amorim@hotmail.com>
2026-09-04 12:06:22 -0300

    feat: Add a PostCoverImage component to display post cover images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostCoverImage } from '@/components/PostCoverImage';
 import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import Image from 'next/image';
-import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
@@ -13,16 +12,16 @@ export default async function HomePage() {
       <Header />
 
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-          <Image
-            className='w-full h-full object-cover object-center group-hover:scale-105 transition duration-300'
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt='Titulo do Post'
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{ href: '/post/asdfasdf' }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: '/images/bryen_9.png',
+            alt: 'Imagem de capa',
+            priority: true,
+          }}
+        />
         <div className='flex flex-col gap-4 sm:justify-center'>
           <time
             className='text-slate-600 block text-sm/tight'
