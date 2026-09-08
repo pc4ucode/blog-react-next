e6699c1a22c828a50021407a90e89a65f635e724
euler.amorim <euler.amorim@hotmail.com>
2026-09-08 11:13:16 -0300

    feat: Implement the findAllPublic function to retrieve public posts and update the PostFeatured and PostsList components

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,10 +1,12 @@
 import { PostCoverImage } from '../PostCoverImage';
-import { PostHeading } from '../PostHeading';
 import { PostSummary } from '../PostSummary';
+import { findAllPublicPosts } from '@/lib/post/queries';
 
-export function PostFeatured() {
-  const slug = 'asdfasdf';
-  const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+
+  const postLink = `/post/${post.slug}`;
 
   return (
     <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
@@ -13,19 +15,17 @@ export function PostFeatured() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: '/images/bryen_9.png',
-          alt: 'Imagem de capa',
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
       <PostSummary
         postLink={postLink}
         postHeading='h1'
-        createdAt={'2025-04-08T00:24:38.616Z'}
-        title={'Rotina de pessoas altamente eficazes'}
-        excerpt={
-          'O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO.'
-        }
+        createdAt={post.createdAt}
+        title={post.title}
+        excerpt={post.excerpt}
       />
     </section>
   );
