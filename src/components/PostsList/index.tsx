b0b28a2cd757ec8b0bf0f02446a34f1c60db4172
euler.amorim <euler.amorim@hotmail.com>
2026-09-08 09:35:59 -0300

    feat: Update the date formatting in posts using utility functions

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,6 +1,7 @@
 import { postRepository } from '@/repositories/post';
 import { PostCoverImage } from '../PostCoverImage';
 import { PostHeading } from '@/components/PostHeading';
+import { formatDateTime, formatDistanceToNow } from '@/utils/format-datetime';
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
@@ -24,8 +25,9 @@ export async function PostsList() {
               <time
                 className='text-slate-600 block text-sm/tight'
                 dateTime={post.createdAt}
+                title={formatDistanceToNow(post.createdAt)}
               >
-                {post.createdAt}
+                {formatDateTime(post.createdAt)}
               </time>
               <PostHeading as='h2' url={postLink}>
                 {post.title}
--- src/utils/format-datetime.ts
+++ src/utils/format-datetime.ts
@@ -1,4 +1,7 @@
-import { format, formatDistanceToNow } from 'date-fns';
+import {
+  format,
+  formatDistanceToNow as dateFnsFormatDistanceToNow,
+} from 'date-fns';
 import { ptBR } from 'date-fns/locale/pt-BR';
 
 export function formatDateTime(rawDate: string): string {
@@ -7,12 +10,8 @@ export function formatDateTime(rawDate: string): string {
   return format(date, "dd/MM/yyyy 'às' HH'h'mm", { locale: ptBR });
 }
 
-export function formatRelativeDate(rawDate: string): string {
+export function formatDistanceToNow(rawDate: string): string {
   const date = new Date(rawDate);
 
-  return formatDistanceToNow(date, { locale: ptBR, addSuffix: true });
+  return dateFnsFormatDistanceToNow(date, { locale: ptBR, addSuffix: true });
 }
-
-const rawDate = '2026-09-08T12:10:48.138Z';
-console.log(rawDate);
-console.log(formatRelativeDate(rawDate));
