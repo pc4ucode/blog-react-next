be9bae984acfb4df28b4b4d7871086d328bc1737
euler.amorim <euler.amorim@hotmail.com>
2026-09-10 10:57:55 -0300

    feat: Update the homepage layout to include the header and container, and improve the HTML structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from '@/components/Container';
-import { Header } from '@/components/Header';
 import { PostFeatured } from '@/components/PostFeatured';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
@@ -7,9 +5,7 @@ import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
@@ -17,10 +13,6 @@ export default async function HomePage() {
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <h1 className='text-6xl font-bold text-center py-8'>FOOTER</h1>
-      </footer>
-    </Container>
+    </>
   );
 }
